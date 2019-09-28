71f742b3c274f9a5108ca73b8c75e9df0b1a4dbb
Jinesh Modi <jinesh1994@gmail.com>
2019-09-28 16:30:26 -0500

    Connecting to firebase

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request, Response,json
+from db import FireBaseConnection
 
 app = Flask(__name__)
 
@@ -9,8 +10,13 @@ def hello():
     }
     js = json.dumps(data)
 
+    connect()
+
     return Response(js, status=200, mimetype='application/json')
 
+def connect():
+    fire_base_connection = FireBaseConnection()
+    fire_base_connection.connect_to_db()
 
 if __name__ == '__main__':
     app.run()
--- db.py
+++ db.py
@@ -1,1 +1,19 @@
 # This file is for all DB connections and DB logic
+
+import pyrebase
+
+class FireBaseConnection:
+    def connect_to_db(self):
+        fire_base_config = {
+            "apiKey": "AIzaSyAwC4JLIC_hS_ODvHr_2oaw7e__P_-PzLY",
+            "authDomain": "hackuta19-notoday.firebaseapp.com",
+            "databaseURL": "https://hackuta19-notoday.firebaseio.com",
+            "projectId": "hackuta19-notoday",
+            "storageBucket": "hackuta19-notoday.appspot.com",
+            "messagingSenderId": "63356394783",
+            "appId": "1:63356394783:web:9e653b492c50427c14e42a",
+            "serviceAccount": "config.json"
+        }
+        fire_base = pyrebase.initialize_app(fire_base_config)
+        # result = fire_base.get('/users', None)
+        print(fire_base)
